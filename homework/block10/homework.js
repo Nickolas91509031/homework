68a0fd59b47c18848613a96c83d023ffe48c0e63
Mykola Marchenko <nickolay91509031@gmail.com>
2023-06-09 16:36:50 +0200

    Update homework.js

--- homework/block10/homework.js
+++ homework/block10/homework.js
@@ -1,4 +1,7 @@
-/*const superImportantArray = [9, 2, 4, 5, 7, 0, 8, 6, 1];
+//#1
+const superImportantArray = [9, 2, 4, 5, 7, 0, 8, 6, 1];
+
+console.log(superImportantArray);
 
 function getMissingElement(superImportantArray) {
   for (let i = 0; i < 10; i++) {
@@ -9,7 +12,56 @@ function getMissingElement(superImportantArray) {
 }
 
 console.log(getMissingElement(superImportantArray));
-*/
+
+//#2
+
+var questions = [
+  {
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+  },
+  {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0,
+  },
+];
+
+console.log(questions);
+
+questions.forEach(function (i) {
+  i.userAnswer = null;
+});
+
+console.log(questions);
+
+//#3
+
+var birthday = [January, February, March, April, May, June, July, August, September, October, November, December, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(birthday);
+
+function getVillainName(birthday) {
+  firstNames = [
+    "The Evil",
+    "The Vile",
+    "The Cruel",
+    "The Trashy",
+    "The Despicable",
+    "The Embarrassing",
+    "The Disreputable",
+    "The Atrocious",
+    "The Twirling",
+    "The Orange",
+    "The Terrifying",
+    "The Awkward",
+  ];
+  lastNames = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"];
+  return firstNames[birthday.getMonth()] + " " + lastNames[birthday.getDate() % 10];
+}
+
+//#4
 
 walk = ["n", "s", "w", "e", "n", "s", "w", "e", "n"];
 
@@ -17,7 +69,7 @@ console.log(walk);
 
 function isValidWalk(walk) {
   for (let i = 0; i < walk.length; i++) {
-    if ((i === 9)) {
+    if (i === 9) {
       return true;
     }
     return false;
