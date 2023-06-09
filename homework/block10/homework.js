5f51648dde6d543338542e7a2d4b998ecf7d63a7
Mykola Marchenko <nickolay91509031@gmail.com>
2023-06-09 15:41:13 +0200

    Update homework.js

--- homework/block10/homework.js
+++ homework/block10/homework.js
@@ -1,21 +1,28 @@
-const superImportantArray = [0, 5, 1, 3, 2, 9, 7, 6, 4];
-console.log(superImportantArray);
-
-function includes(arr, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
+/*const superImportantArray = [9, 2, 4, 5, 7, 0, 8, 6, 1];
 
 function getMissingElement(superImportantArray) {
   for (let i = 0; i < 10; i++) {
-    if (includes(superImportantArray, i) === false) {
+    if (!superImportantArray.includes(i)) {
       return i;
     }
   }
 }
 
 console.log(getMissingElement(superImportantArray));
+*/
+
+walk = ["n", "s", "w", "e", "n", "s", "w", "e", "n", "s"];
+
+console.log(walk);
+
+function isValidWalk(walk) {
+  for (i = 0; i < walk.length; i++) {
+    if ((i = 9)) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+}
+
+console.log(isValidWalk(walk));
