08519ee527124f8d70a505c951137714f549b8db
Mykola Marchenko <nickolay91509031@gmail.com>
2023-06-14 22:39:11 +0200

    commit

--- homework/block10/homework.js
+++ homework/block10/homework.js
@@ -36,61 +36,26 @@ questions.forEach(function (i) {
 
 console.log(questions);
 
-//#3
-
-var birthday = [January, February, March, April, May, June, July, August, September, October, November, December, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-console.log(birthday);
-
-function getVillainName(birthday) {
-  firstNames = [
-    "The Evil",
-    "The Vile",
-    "The Cruel",
-    "The Trashy",
-    "The Despicable",
-    "The Embarrassing",
-    "The Disreputable",
-    "The Atrocious",
-    "The Twirling",
-    "The Orange",
-    "The Terrifying",
-    "The Awkward",
-  ];
-  lastNames = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"];
-  return firstNames[birthday.getMonth()] + " " + lastNames[birthday.getDate() % 10];
-}
-
-console.log(getVillainName(birthday));
-
 //#4
 
 const walk = ["n", "s", "w", "e", "n", "s", "w", "e", "n", "s"];
 
-console.log(walk);
-
-let north = 0;
-let south = 0;
-let east = 0;
-let west = 0;
-
 function isValidWalk(walk) {
-  for (let i = 0; i < walk.length; i++) {
-    if (walk[i] == 'n') {
-      north = 1;
-    } else if (walk[i] == 's') {
-      south = -1;
-    } else if (walk[i] == 'w') {
-      west = 1;
-    } else (walk[i] == 'e') {
-      east = -1;
-    }
+  if (walk.length !== 10) {
+    return false;
   }
 
-function result(walk) {
-  if (walk.isValidWalk(walk.reduce) = 0) {
-    return true;
+  for (let i = 0; i < walk.length; i++) {
+    if (walk[i] === "n") {
+      north = 1;
+    } else if (walk[i] === "s") {
+      south = 1;
+    } else if (walk[i] === "w") {
+      west = 1;
+    } else if (walk[i] === "e") {
+      east = 1;
+    }
   }
 }
 
-  console.log(isValidWalk(walk));
+return north === south && west === east;
