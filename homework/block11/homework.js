0bd4943f46f54945f627450a53700671fe4397e7
Mykola Marchenko <nickolay91509031@gmail.com>
2023-06-22 20:33:32 +0200

    Update homework.js

--- homework/block11/homework.js
+++ homework/block11/homework.js
@@ -18,7 +18,7 @@ console.log(getPeople(3)); // 2
 console.log(getPeople(6)); // 3
 console.log(getPeople(10)); // 4
 console.log(getPeople(15)); // 5
-console.log(getPeople(12)); // ? 15
+console.log(getPeople(120)); // ? 15
 
 //  6           7
 // 15 + 6 = 21 + 7 = 28 + 8 = 36 + 9 = 45 + 10 = 55 + 11 = 66 + 12 = 78 + 13 = 91 + 14 = 105 + 15 = 120;
