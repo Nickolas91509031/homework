01a2d0099e169446368b165ceff0fed76afec11f
Mykola Marchenko <nickolay91509031@gmail.com>
2023-06-27 18:42:32 +0200

    commit

--- homework/timeToDay/script.js
+++ homework/timeToDay/script.js
@@ -1,5 +1,6 @@
 const current = new Date();
-let left = 0;
+let monthes = 0;
+let years = 0;
 let text = "";
 
 const dateInput = prompt("Please enter the date (YYYY-MM-DD)");
@@ -7,22 +8,22 @@ const dateInput = prompt("Please enter the date (YYYY-MM-DD)");
 const inputDate = new Date(dateInput);
 const timeDiff = inputDate.getTime() - current.getTime();
 
-/*
-if (dateInput = "") {
+if (isNaN(inputDate)) {
     text = "Something's wrong";
 }
-*/
 
-if (timeDiff < 0) {
-    left = Math.ceil((timeDiff - 1000*60*60*24*30) / (1000 * 60 * 60 * 24 * 30 * 12));
+else if (timeDiff < 0) {
+    years = Math.ceil((timeDiff - 1000*60*60*24*30) / (1000 * 60 * 60 * 24 * 30 * 12));
+    monthes = Math.ceil((timeDiff - 1000*60*60*24*30) % (1000 * 60 * 60 * 24 * 30 * 12) / (1000 * 60 * 60 * 24 * 30));
 
-    text = "That's was " + (-left) + " years old";
+    text = "That's was " + (-years) + " years and " + (-monthes) + " monthes old";
 }
 
 else {
-    left = Math.ceil((timeDiff - 1000*60*60*24*30) / (1000 * 60 * 60 * 24 * 30));
+    years = Math.ceil((timeDiff - 1000*60*60*24*30) / (1000 * 60 * 60 * 24 * 30 * 12));
+    monthes = Math.ceil((timeDiff - 1000*60*60*24*30) % (1000 * 60 * 60 * 24 * 30 * 12) / (1000 * 60 * 60 * 24 * 30));
 
-    text = "For your date left " + left + " monthes";
+    text = "For your date left " + (years) + " years and " + (monthes) + " monthes";
 }
 
 document.getElementById("date").innerHTML = text;
